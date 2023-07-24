820f41e5338c9feb1c2102ea0a613e4795213a1a
SImon Njenga <makuno.biz@gmail.com>
2023-07-24 05:54:45 +0300

    Firebase and client class creation

---
+++ app/src/main/java/com/peammobility/classes/Client.java
@@ -0,0 +1,57 @@
+package com.peammobility.classes;
+
+public class Client {
+    String name,email,phoneNumber,createdAT;
+    Boolean isActive;
+
+    public Client() {
+    }
+
+    public Client(String name, String email, String phoneNumber, String createdAT, Boolean isActive) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.createdAT = createdAT;
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCreatedAT() {
+        return createdAT;
+    }
+
+    public void setCreatedAT(String createdAT) {
+        this.createdAT = createdAT;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+}
---
+++ app/src/main/java/com/peammobility/firebase/Auth.java
@@ -0,0 +1,11 @@
+package com.peammobility.firebase;
+
+public class Auth {
+    public static final String CLIENTS_TABLE = "clients";
+
+    public boolean registerClient(){
+
+
+        return  true;
+    }
+}
