e2c2cef8ac9389f402596b5981b44ae04450e279
Simon Njenga <makuno.biz@gmail.com>
2023-08-04 03:55:31 +0300

    untrack .idea

--- app/src/androidTest/java/com/peammobility/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/peammobility/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ecab", appContext.packageName)
+        assertEquals("com.peammobility", appContext.packageName)
     }
 }
